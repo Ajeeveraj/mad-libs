635b820d6aaed01ce8d8d9190d102a11be4efc79
Ajeeveraj <ayushj762@deltalearns.ca>
2025-06-13 11:20:05 -0700

    mostly done
    finished story

---
+++ main.py/main.py
@@ -0,0 +1,22 @@
+#starting the game
+print("Fill out all of the spaces.")
+
+# Variables
+You = input("Your name is ")
+Animal= input("Animal name : ")
+Profession = input("Profession name ")
+Vehichle = input("Vehichle name ")
+Place = input("Place name ")
+Noun = input("Noun name ")
+Verb = input("Verb name ")
+Noun2 = input("Noun name ")
+Emotion = input("Emotion name ")
+Verb2 = input("Verb name ")
+    
+    
+
+
+print(f"One day {You} slapped a {Animal} in {Place} after going home from your work as {Profession}")
+print(f" your {Vehichle}. But then suddenly a {Noun} decided to {Verb} and smashed your {Vehichle}.")
+print(f"This made your friend the {Noun2} feel {Emotion} and he decided to {Verb2}")
+    
