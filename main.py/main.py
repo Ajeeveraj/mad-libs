bdccc75edfc80e6aced2bf4731ed153fd3196136
Ajeeveraj <ayushj762@deltalearns.ca>
2025-06-16 13:28:12 -0700

    done
    done

--- main.py/main.py
+++ main.py/main.py
@@ -1,22 +1,22 @@
 #introduction
-print("Fill out all of the spaces.")
+print("Fill out all of the spaces to complete the mad libs.")
 
 #Variables
-You = input("Your name is ")
+You = input("Your name is ").capitalize()
 Animal= input("Animal name : ")
 Profession = input("Profession name ")
-Vehichle = input("Vehichle name ")
+Vehicle = input("Vehicle name ")
 Place = input("Place name ")
-Noun = input("Noun name ")
-Verb = input("Verb name ")
-Noun2 = input("Noun name ")
+Noun = input("person,animal, or thing name ")
+Verb = input("Past tense verb name ")
+Noun2 = input("Person,animal, or thing name ").capitalize()
 Emotion = input("Emotion name ")
-Verb2 = input("Verb name ")
+past_tense_Verb2 = input("present tense Verb name ")
     
     
 #starting the game
 
-print(f"One day {You} slapped some {Animal}s in {Place} after going home from your work as {Profession}")
-print(f"riding his {Vehichle}. But then suddenly a {Noun} got mad and decided to {Verb} at him and smashed his {Vehichle}.")
-print(f"This made his friend the {Noun2} feel {Emotion} and he decided to {Verb2} and charged at the {Noun}")
+print(f"One day {You} slapped some {Animal}s in {Place} after going home from his work as a {Profession},")
+print(f"riding his {Vehicle}. But then suddenly a {Noun} got mad and {Verb} at him and smashed his {Vehicle}.")
+print(f"This made his friend the {Noun2} feel {Emotion} and he decided to {past_tense_Verb2} the {Noun}.")
     
